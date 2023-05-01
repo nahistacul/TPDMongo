c6ddef6f847084d848c81e82326d96199af0e09c
nahistacul <nahiarastacul@gmail.com>
2023-05-01 00:33:18 -0300

    bdd y server configurados

---
+++ app.js
@@ -0,0 +1,50 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
+
+
+// Connection to DB
+
+mongoose.connect('mongodb://localhost/tvshows');
+const database = mongoose.connection;
+
+database.once('connected', () => {
+  console.log('Connected to Database');
+});
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Import Models and controllers
+var models= require('./models/tvshow.js')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshow.js');
+
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
+});
+app.use(router);
+
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
